b041dcb1976b0bbaab869dc1eba722883a7d98ae
Ayo <Ayoroq@gmail.com>
2025-07-07 19:51:42 -0400

    renaming the functions for clarity and ching from users to Astronauts

--- exercises_book/cli/who_is_in_space.mjs
+++ exercises_book/cli/who_is_in_space.mjs
@@ -31,34 +31,34 @@ function getLongestValues(data) {
   return { longestName, longestCraftName };
 }
 
-// function to group the users by craft
-function groupUsers(data) {
+// function to group the astronaut by craft
+function groupAstronauts(data) {
   const { count, astronauts } = data;
-  const groupedUsers = Object.groupBy(astronauts, (people) => people.craft);
-  return groupedUsers;
+  const groupedAstronauts = Object.groupBy(astronauts, (people) => people.craft);
+  return groupedAstronauts;
 }
 
-// function to return the sorted users
-function sortGroupedUsers(groupedUsers) {
-  const sortedUsers = {};
-  for (const [craft, people] of Object.entries(groupedUsers)) {
-    sortedUsers[craft] = people.sort((a, b) => {
+// function to return the sorted astronaut
+function sortGroupedAstronauts(groupedAstronauts) {
+  const sortedAstronauts = {};
+  for (const [craft, people] of Object.entries(groupedAstronauts)) {
+    sortedAstronauts[craft] = people.sort((a, b) => {
       const lastA = a.name.split(" ").at(-1);
       const lastB = b.name.split(" ").at(-1);
       return lastA.localeCompare(lastB);
     });
   }
-  return sortedUsers;
+  return sortedAstronauts;
 }
 
-// function to print information about the users
-function printUsers(data) {
+// function to print information about the astronauts
+function printAstronauts(data) {
   const { longestName, longestCraftName } = getLongestValues(data);
-  const groupedUsers = groupUsers(data);
-  const sortedUsers = sortGroupedUsers(groupedUsers);
+  const groupedAstronauts = groupAstronauts(data);
+  const sortedAstronauts = sortGroupedAstronauts(groupedAstronauts);
   console.log(`${"Name".padEnd(longestName)} | ${"Craft".padEnd(longestCraftName)}`);
   console.log("-".repeat(longestName + longestCraftName + 4));
-  Object.entries(sortedUsers).forEach(([craft, people]) => {
+  Object.entries(sortedAstronauts).forEach(([craft, people]) => {
     for (const person of people) {
       const nameCell = person.name.padEnd(longestName);
       // Only print craft for the first person in the group
@@ -75,7 +75,7 @@ async function main() {
     const data = await getAstronauts();
     if (data.count > 0) {
       console.log(`\nThere are ${data.count} astronauts in space right now:\n`);
-      printUsers(data);
+      printAstronauts(data);
     } else {
       console.log("Could not retrieve astronaut information or no one is in space.");
     }
