63c24e820c6b0f44cad219cdf29b45e278da6bce
Ayo <Ayoroq@gmail.com>
2025-07-23 19:13:02 -0400

    Add movie details and layout to main section

--- exercises_book/gui/movie_tv_search/search.mjs
+++ exercises_book/gui/movie_tv_search/search.mjs
@@ -5,6 +5,7 @@ async function searchMedia(query) {
   try {
     query = query?.trim();
     const response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent(query)}&api_key=${apiKey}`);
+    
     const data = await response.json();
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -15,11 +16,20 @@ async function searchMedia(query) {
   }
 }
 
+// id: 438631,
 // to get the images 
 // poster - https://image.tmdb.org/t/p/w45/d5NXSklXo0qyIYkgV94XAgMIckC.jpg
 // backdrop - https://image.tmdb.org/t/p/original/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg
 
-const results = await searchMedia("Rye Lane");
-console.log(results);
+// const results = await searchMedia("Dune");
+// console.log(results);
+
+async function test () {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/438631/credits?api_key=${apiKey}`);
+   const data = await response.json();
+   console.log(data.cast.slice(0,10));
+}
 // Replace with your actual API key
 // Note: Ensure to handle the API key securely in production environments.
+
+await test();
