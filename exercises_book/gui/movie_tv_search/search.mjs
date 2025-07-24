b36490c38af767dd32ce93585af608031a5282fe
Ayo <Ayoroq@gmail.com>
2025-07-24 13:48:30 -0400

    Add prototype dropdown menu for movie suggestions and enhance styling

--- exercises_book/gui/movie_tv_search/search.mjs
+++ exercises_book/gui/movie_tv_search/search.mjs
@@ -10,7 +10,17 @@ async function searchMedia(query) {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return data.results
+    return data.results.slice(0, 7).map(item => ({
+      id: item.id,
+      title: item.original_title || item.title,
+      poster: item.poster_path,
+      backdrop: item.backdrop_path,
+      overview: item.overview,
+      releaseDate: item.release_date || '',
+      voteAverage: item.vote_average,
+  
+    }));
+
   } catch (error) {
     console.error("Error searching media:", error);
   }
@@ -21,15 +31,15 @@ async function searchMedia(query) {
 // poster - https://image.tmdb.org/t/p/w45/d5NXSklXo0qyIYkgV94XAgMIckC.jpg
 // backdrop - https://image.tmdb.org/t/p/original/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg
 
-// const results = await searchMedia("Dune");
-// console.log(results);
+const results = await searchMedia("Dune");
+console.log(results);
 
-async function test () {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/438631/credits?api_key=${apiKey}`);
-   const data = await response.json();
-   console.log(data.cast.slice(0,10));
-}
+// async function test () {
+//   const response = await fetch(`https://api.themoviedb.org/3/movie/438631/credits?api_key=${apiKey}`);
+//    const data = await response.json();
+//    console.log(data.cast.slice(0,10));
+// }
 // Replace with your actual API key
 // Note: Ensure to handle the API key securely in production environments.
 
-await test();
+// await test();
